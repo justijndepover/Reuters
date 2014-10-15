085337d32eb82ba79dd194ba33bd879bf8a75e2f
arnvanhoutte <arnvanhoutte@hotmail.com>
2014-10-15 09:53:12 +0200

    Basis app
    Standaard WP Panorama app

---
+++ Reuters/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Reuters.Resources;
+
+namespace Reuters
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
